051c71ae4536c83ba13e271baf76918a081c5504
NeptuneBlue42 <157250671+NeptuneBlue42@users.noreply.github.com>
2026-01-30 21:29:41 -0600

    fixed leading 0  for seconds in level timer

--- scenes/timer_display.gd
+++ scenes/timer_display.gd
@@ -10,6 +10,9 @@ func _process(delta: float) -> void:
 	var time_left = (int(timer.get_time_left()))
 	var minutes_left: int = int(time_left / 60)
 	var seconds_left: int = time_left % 60
-	var display_time = str(minutes_left) + ":" + str(seconds_left)
+	var middle_chars = ":"
+	if seconds_left < 10:
+		middle_chars += "0"
+	var display_time = str(minutes_left) + middle_chars + str(seconds_left)
 	set_text(display_time)
 	pass
